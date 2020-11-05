0944c0c331444bef0bc704d861f908031f2c4743
yangwenpeng <wb-ywp747121@alibaba-inc.com>
2020-11-05 17:36:34 +0800

    添加注释

--- 手写MVCC/src/mvcc/MVCC.java
+++ 手写MVCC/src/mvcc/MVCC.java
@@ -3,6 +3,9 @@ package mvcc;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * MVCC核心逻辑
+ */
 public class MVCC {
 
     // 1:脏读 2：读提交 3：可重复读 4：串行
@@ -12,12 +15,25 @@ public class MVCC {
 
     private Map<Long, Set<Node>> transactionMap = new ConcurrentHashMap<>();
 
+    /**
+     * 创建事务
+     *
+     * @return 事务ID
+     */
     public long startTransaction() {
         return System.currentTimeMillis() + (long) (Math.random() * 1000);
     }
 
+    /**
+     * 提交事务
+     *
+     * @param transactionId 事务ID
+     */
     public void commit(long transactionId) {
-        Set<Node> updateNodes = transactionMap.get(transactionId);
+        Set<Node> updateNodes = transactionMap.remove(transactionId);
+        if (updateNodes == null) {
+            return;
+        }
         updateNodes.forEach(node -> {
             node.commit = true;
             node.line.node = node;
@@ -28,6 +44,14 @@ public class MVCC {
         });
     }
 
+    /**
+     * 读取某一行数据<br>
+     * read a line
+     *
+     * @param lineNum       行数(number of line)
+     * @param transactionId 事务ID(the id of transaction)
+     * @return
+     */
     public Object read(int lineNum, long transactionId) {
         if (lineNum >= table.size()) {
             return null;
@@ -60,12 +84,23 @@ public class MVCC {
         return -1;
     }
 
+    /**
+     * 更新某一行数据<br>
+     * update a line
+     *
+     * @param value         该行新数据(data of line)
+     * @param lineNum       行号(number of line)
+     * @param transactionId 事务ID(the id of transaction)
+     * @return
+     */
     public boolean update(Object value, int lineNum, long transactionId) {
         if (lineNum >= table.size()) {
             return false;
         }
         Line line = table.get(lineNum);
         long updateId = line.updateId.get();
+
+        // 如果不持有行锁，则尝试获取行锁
         if (updateId != transactionId) {
             while (!line.updateId.compareAndSet(0, transactionId)) {
                 try {
@@ -77,12 +112,25 @@ public class MVCC {
                 }
             }
         }
+
         Node node = flushMyNode(line.node, value, transactionId);
+
+        // 记录当前事务更新的所有行，便于提交
         Set<Node> nodeList = transactionMap.computeIfAbsent(transactionId, k -> new HashSet<>());
         nodeList.add(node);
+
         return true;
     }
 
+    /**
+     * 在事务回滚链中找到当前事务节点，并且更新数据<br>
+     * Locate the current transaction node in the transaction rollback chain and update the data
+     *
+     * @param node          回滚链起点(Rollback chain start point)
+     * @param value         新数据(the new data)
+     * @param transactionId 事务ID(id of the transaction)
+     * @return
+     */
     private Node flushMyNode(Node node, Object value, long transactionId) {
         Node tail = node;
         while (tail.next != null) {
@@ -103,6 +151,14 @@ public class MVCC {
         return tail;
     }
 
+    /**
+     * 插入数据<br>
+     * insert data
+     *
+     * @param value         data
+     * @param transactionId 事务ID(id of the transaction)
+     * @return
+     */
     public synchronized int insert(Object value, long transactionId) {
         Node node = new Node(0, null, null, value, true);
         node.isUpdate = true;
